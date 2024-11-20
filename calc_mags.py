58cfed98565e9566ea5a665683a3335c8983a247
Holden Truman <143415887+holden-truman@users.noreply.github.com>
2024-11-20 17:08:13 +0000

    saving

--- calc_mags.py
+++ calc_mags.py
@@ -34,36 +34,28 @@ def calc_mag(cat, mags, flux_cols, flux_errs):
 file_list = glob('flux_files/*')
 print("running")
 for file in file_list:
-        print(file)
-
-	file_name = file[len('flux_files/'):]
-	print(file_name)
-
-	cat = Table.read(file, format='fits')
-
-	mags, flux_cols, flux_errs = get_filter_name(cat)
-	cat = calc_mag(cat, mags, flux_cols, flux_errs)
-
-	for mag in mags:
-		plt.scatter(cat[mag], cat[mag+'_err'], s=5, label=mag)
-
-	plt.legend(fontsize=14)
-	plt.xlabel('Magnitudes', fontsize=14)
-	plt.ylabel('Mag Err', fontsize=14)
-	plt.title(file_name)
-	plt.savefig(file_name + '.png')
-	plt.clf()
-
-	for mag in mags:
-		ind, = np.where(cat[mag+'_err'] > 0.1)
-		cat.remove_rows(ind)
-		plt.scatter(cat[mag], cat[mag+'_err'], s=5, label=mag)
-
-	plt.legend(fontsize=14)
-	plt.xlabel('Magnitudes', fontsize=14)
-	plt.ylabel('Mag Err', fontsize=14)
-	plt.title(file_name + ' Mag Err < 0.1')
-	plt.savefig(file_name + '_clipped.png')
-	plt.clf()
-
-	cat.write('mag_files/' + file_name, format='fits', overwrite=True)
+    print(file)
+    file_name = file[len('flux_files/'):]
+    print(file_name)
+    cat = Table.read(file, format='fits')
+    mags, flux_cols, flux_errs = get_filter_name(cat)
+    cat = calc_mag(cat, mags, flux_cols, flux_errs)
+    for mag in mags:
+        plt.scatter(cat[mag], cat[mag+'_err'], s=5, label=mag)
+    plt.legend(fontsize=14)
+    plt.xlabel('Magnitudes', fontsize=14)
+    plt.ylabel('Mag Err', fontsize=14)
+    plt.title(file_name)
+    plt.savefig(file_name + '.png')
+    plt.clf()
+    for mag in mags:
+        ind, = np.where(cat[mag+'_err'] > 0.1)
+        cat.remove_rows(ind)
+        plt.scatter(cat[mag], cat[mag+'_err'], s=5, label=mag)
+    plt.legend(fontsize=14)
+    plt.xlabel('Magnitudes', fontsize=14)
+    plt.ylabel('Mag Err', fontsize=14)
+    plt.title(file_name + ' Mag Err < 0.1')
+    plt.savefig(file_name + '_clipped.png')
+    plt.clf()
+    cat.write('mag_files/' + file_name, format='fits', overwrite=True)
