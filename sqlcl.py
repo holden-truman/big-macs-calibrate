fb6b058a97189e4f0ce545424b7d8d51feea2f75
Holden Truman <143415887+holden-truman@users.noreply.github.com>
2024-11-20 02:04:12 +0000

    saving

--- sqlcl.py
+++ sqlcl.py
@@ -170,7 +170,7 @@ def panstarrs_ebv(lon, lat, coordsys='equ', mode='full'): #holden# problem here,
     from astropy.coordinates import SkyCoord
     import astropy.units as units
     from dustmaps.sfd import SFDQuery
-    sfd = SFDQuery(map_fname="/fs/ddn/sdf/group/kipac/u/awright/bayestar2019.h5")
+    sfd = SFDQuery()
     coords = SkyCoord(ra=lon*units.deg, dec=lat*units.deg,
                     frame='icrs')
 
