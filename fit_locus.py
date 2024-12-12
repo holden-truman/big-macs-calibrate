a3c0f13447cb045ba2c51094465c1c85c2a066ad
Holden Truman <143415887+holden-truman@users.noreply.github.com>
2024-12-12 20:38:28 +0000

    saving

--- fit_locus.py
+++ fit_locus.py
@@ -208,7 +208,7 @@ def get_survey_stars(file, inputcat, racol, deccol, necessary_columns, EBV, surv
                             FROM gaiadr3.gaia_source \
                             WHERE 1=CONTAINS( POINT('ICRS',ra,dec), BOX('ICRS'," + str(RA) + "," + str(DEC) + "," + str(RAD) + ", " + str(RAD) + ")) \
                             AND phot_g_mean_mag<=19 AND phot_bp_mean_mag>=5 AND phot_rp_mean_mag>=5 \
-                            AND phot_bp_rp_excess_factor > (1.0 + 0.015*bp_rp*bp_rp) AND phot_bp_rp_excess_factor < (1.3 + 0.06*bp_rp*bp_rp) ", (DR)
+                            AND phot_bp_rp_excess_factor > (1.0 + 0.015*bp_rp*bp_rp) AND phot_bp_rp_excess_factor < (1.3 + 0.06*bp_rp*bp_rp) "
                             ## AND bp_rp >  0.6 AND bp_rp < 1.6 "
         print(query)
         
