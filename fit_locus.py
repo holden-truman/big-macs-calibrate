46d7961c910b081720325218421497a76684c0c0
Holden Truman <143415887+holden-truman@users.noreply.github.com>
2025-01-07 04:29:49 +0000

    saving

--- fit_locus.py
+++ fit_locus.py
@@ -238,33 +238,25 @@ def get_survey_stars(file, inputcat, racol, deccol, necessary_columns, EBV, surv
             """
             query = f"""
             WITH stats AS (
-                SELECT AVG(
-                    ESDC_CASE_CONDITION(
+                SELECT AVG(c_star) AS mean, STDDEV(c_star) AS stddev
+                FROM (
+                    SELECT ESDC_CASE_CONDITION(
                         1.162004 + 0.011464 * bp_rp + 0.049255 * bp_rp * bp_rp - 0.005879 * bp_rp * bp_rp * bp_rp,
                         bp_rp >= 4, 1.057572 + 0.1405537 * bp_rp, 
                         bp_rp < 0.5, 1.154360 + 0.033772 * bp_rp + 0.32277 * bp_rp * bp_rp
-                    )
-                ) AS mean,
-                STDDEV(
-                    ESDC_CASE_CONDITION(
-                        1.162004 + 0.011464 * bp_rp + 0.049255 * bp_rp * bp_rp - 0.005879 * bp_rp * bp_rp * bp_rp,
-                        bp_rp >= 4, 1.057572 + 0.1405537 * bp_rp, 
-                        bp_rp < 0.5, 1.154360 + 0.033772 * bp_rp + 0.32277 * bp_rp * bp_rp
-                    )
-                ) AS stddev
-                FROM gaiadr3.gaia_source
-                WHERE phot_g_mean_mag <= 22
-                AND phot_bp_mean_mag >= 5
-                AND phot_rp_mean_mag >= 5
+                    ) AS c_star
+                    FROM gaiadr3.gaia_source
+                )
             )
             SELECT dr3.ra, dr3.dec, dr3.bp_rp,
                 dr3.phot_g_mean_flux, dr3.phot_g_mean_flux_error,
                 dr3.phot_bp_mean_flux, dr3.phot_bp_mean_flux_error,
                 dr3.phot_rp_mean_flux, dr3.phot_rp_mean_flux_error,
-                ESDC_CASE_CONDITION(    1.162004+.011464*bp_rp+0.049255*bp_rp*bp_rp-0.005879*bp_rp*bp_rp*bp_rp,
-                    bp_rp >= 4,         1.057572+0.1405537*bp_rp, 
-                    bp_rp < 0.5,        1.154360+0.033772*bp_rp+0.32277*bp_rp*bp_rp) 
-                AS c_star
+                ESDC_CASE_CONDITION(
+                    1.162004 + 0.011464 * bp_rp + 0.049255 * bp_rp * bp_rp - 0.005879 * bp_rp * bp_rp * bp_rp,
+                    bp_rp >= 4, 1.057572 + 0.1405537 * bp_rp, 
+                    bp_rp < 0.5, 1.154360 + 0.033772 * bp_rp + 0.32277 * bp_rp * bp_rp
+                ) AS c_star
             FROM gaiadr3.gaia_source AS dr3, stats
             WHERE 1 = CONTAINS(
                     POINT('ICRS', ra, dec),
@@ -274,9 +266,10 @@ def get_survey_stars(file, inputcat, racol, deccol, necessary_columns, EBV, surv
             AND phot_bp_mean_mag >= 5
             AND phot_rp_mean_mag >= 5
             AND c_star BETWEEN stats.mean - 3 * stats.stddev
-                AND stats.mean + 3 * stats.stddev
+                            AND stats.mean + 3 * stats.stddev
             {color_range}
             """
+
 
             #465 stars with no c_star filter at mag 22 cut
 
