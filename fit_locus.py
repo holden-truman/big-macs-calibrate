a773ef3925e391bc8f6d3e28600f321078b9f548
Holden Truman <143415887+holden-truman@users.noreply.github.com>
2025-01-07 04:44:08 +0000

    saving

--- fit_locus.py
+++ fit_locus.py
@@ -245,9 +245,9 @@ def get_survey_stars(file, inputcat, racol, deccol, necessary_columns, EBV, surv
                     dr3.phot_rp_mean_flux, dr3.phot_rp_mean_flux_error,
                     dr3.source_id,
                     ESDC_CASE_CONDITION(
-                        1.162004 + 0.011464 * bp_rp + 0.049255 * bp_rp * bp_rp - 0.005879 * bp_rp * bp_rp * bp_rp,
-                        bp_rp >= 4, 1.057572 + 0.1405537 * bp_rp, 
-                        bp_rp < 0.5, 1.154360 + 0.033772 * bp_rp + 0.32277 * bp_rp * bp_rp
+                        phot_bp_rp_excess_factor - (1.162004 + 0.011464 * bp_rp + 0.049255 * bp_rp * bp_rp - 0.005879 * bp_rp * bp_rp * bp_rp),
+                        bp_rp >= 4, phot_bp_rp_excess_factor - (1.057572 + 0.1405537 * bp_rp), 
+                        bp_rp < 0.5, phot_bp_rp_excess_factor - (1.154360 + 0.033772 * bp_rp + 0.32277 * bp_rp * bp_rp)
                     ) AS c_star
                 FROM gaiadr3.gaia_source AS dr3
             )
