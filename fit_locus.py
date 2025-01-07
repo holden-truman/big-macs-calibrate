431d65407c4c8396b3d74671474f78afa54a4320
Holden Truman <143415887+holden-truman@users.noreply.github.com>
2025-01-07 05:20:51 +0000

    saving

--- fit_locus.py
+++ fit_locus.py
@@ -254,8 +254,8 @@ def get_survey_stars(file, inputcat, racol, deccol, necessary_columns, EBV, surv
             AND phot_g_mean_mag <= 22
             AND phot_bp_mean_mag >= 5
             AND phot_rp_mean_mag >= 5
-            AND c_star < 0 + (0.0059898 + 8.817481*10^-12*POW(photo_g_mean_flux,7.618399))
-            AND c_star > 0 - (0.0059898 + 8.817481*10^-12*POW(photo_g_mean_flux,7.618399))
+            AND c_star < (0.0059898 + 8.817481*10^-12*POW(photo_g_mean_flux,7.618399))
+            AND c_star > (0.0059898 + 8.817481*10^-12*POW(photo_g_mean_flux,7.618399))
             {color_range}
             """
             #see https://scholar.google.com/scholar_lookup?title=Gaia+Early+Data+Release+3+-+Photometric+content+and+validation&author=Riello+M.+De+Angeli+F.+Evans+D.+W.&journal=A%26A&volume=649&pages=A3&publication_year=2021&issn=0004-6361%2C1432-0746&doi=10.1051%2F0004-6361%2F202039587
