ad2947f8538bed6dee0ac1f05e95f593fd3e4d96
Holden Truman <143415887+holden-truman@users.noreply.github.com>
2025-01-07 06:58:16 +0000

    saving

--- fit_locus.py
+++ fit_locus.py
@@ -236,7 +236,7 @@ def get_survey_stars(file, inputcat, racol, deccol, necessary_columns, EBV, surv
                             + str(color_range)
                             ## AND bp_rp >  0.6 AND bp_rp < 1.6 "
             """
-            #c_star mean is 0
+            #c_star mean is 0 (the ABS stuff is c_star, the excess factor - parens)
             query = f"""
             SELECT dr3.ra, dr3.dec, dr3.bp_rp,
                 dr3.phot_g_mean_flux, dr3.phot_g_mean_flux_error,
@@ -260,21 +260,7 @@ def get_survey_stars(file, inputcat, racol, deccol, necessary_columns, EBV, surv
             #see https://scholar.google.com/scholar_lookup?title=Gaia+Early+Data+Release+3+-+Photometric+content+and+validation&author=Riello+M.+De+Angeli+F.+Evans+D.+W.&journal=A%26A&volume=649&pages=A3&publication_year=2021&issn=0004-6361%2C1432-0746&doi=10.1051%2F0004-6361%2F202039587
             #sction 9.4 for information about filtering with c_star, stddev is estimated using a power-law
 
-            #ESDC_CASE_CONDITION(    1.162004+.011464*bp_rp+0.049255*bp_rp*bp_rp-0.005879*bp_rp*bp_rp*bp_rp,
-            #       bp_rp >= 4,         1.057572+0.1405537*bp_rp, 
-            #       bp_rp < 0.5,        1.154360+0.033772*bp_rp+0.32277*bp_rp*bp_rp) 
-            #   AS c_star
-            #HAVING
-                #ABS(c_star)<(0.0059898 + 8.817481e-12 * POWER(dr3.phot_g_mean_mag,7.618399))
-
-
-            #AND (
-            #       ( bp_rp >= 4.0 AND ABS(dr3.phot_bp_rp_excess_factor-(1.057572+0.1405537*bp_rp))<3*(0.0059898 + 8.817481e-12 * POWER(dr3.phot_g_mean_mag,7.618399)) ) OR
-            #       ( bp_rp < 0.5 AND ABS(dr3.phot_bp_rp_excess_factor-(1.154360+0.033772*bp_rp+0.32277*bp_rp*bp_rp))<3*(0.0059898 + 8.817481e-12 * POWER(dr3.phot_g_mean_mag,7.618399)) ) OR
-            #       ( bp_rp >= 0.5 AND bp_rp < 4.0 AND ABS(dr3.phot_bp_rp_excess_factor-(1.162004+.011464*bp_rp+0.049255*bp_rp*bp_rp-0.005879*bp_rp*bp_rp*bp_rp))<3*(0.0059898 + 8.817481e-12 * POWER(dr3.phot_g_mean_mag,7.618399))  )
-            #   )
-
-            #465 stars with no c_star filter at mag 22 cut
+            #465 found stars with no c_star filter at mag 22 cut, but bad ZPs ~.45 too big uniformly
 
 
 # Uncomment the following line if additional filters are needed
