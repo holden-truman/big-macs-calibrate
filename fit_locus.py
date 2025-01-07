739a61cbf89082c37979c21854eec902ba63ed8a
Holden Truman <143415887+holden-truman@users.noreply.github.com>
2025-01-07 04:59:57 +0000

    saving

--- fit_locus.py
+++ fit_locus.py
@@ -238,6 +238,22 @@ def get_survey_stars(file, inputcat, racol, deccol, necessary_columns, EBV, surv
             """
             mean_c_star = 0
             stddev_c_star = 0
+            stats_query = f"""
+            WITH all_c_star AS 
+            (
+                SELECT 
+                        ESDC_CASE_CONDITION(    1.162004+.011464*bp_rp+0.049255*bp_rp*bp_rp-0.005879*bp_rp*bp_rp*bp_rp,
+                        bp_rp >= 4,         1.057572+0.1405537*bp_rp, 
+                        bp_rp < 0.5,        1.154360+0.033772*bp_rp+0.32277*bp_rp*bp_rp) 
+                    AS c_star
+                FROM gaiadr.gaia_source
+            )
+            SELECT AVG(c_star) FROM all_c_star
+
+            """
+            stats_job = Gaia.launch_job_async(stats_query)
+            stats_data = stats_job.get_results() #Accessing data from query might be different for DR3 (different units??)
+
             query = f"""
             SELECT dr3.ra, dr3.dec, dr3.bp_rp,
                 dr3.phot_g_mean_flux, dr3.phot_g_mean_flux_error,
