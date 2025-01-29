6d908ae1dd2bf09f7d404631540a5eb813bab43b
Holden Truman <143415887+holden-truman@users.noreply.github.com>
2025-01-29 23:35:13 +0000

    saving

--- fit_locus.py
+++ fit_locus.py
@@ -1382,6 +1382,8 @@ def fit(table, input_info_unsorted, mag_locus,
             if iteration == 'full': 
 
                 if True:
+                    print("input info", input_info)
+                    print("hold input info"hold_input_info)
                     pinit = []                                                                                                               
                     for i in range(len(hold_input_info),len(input_info)):
                         key = input_info[i]['mag'] ## varying magnitudes
