f02df804f2a2ed76aa4d24114135ebd0851a626f
Holden Truman <143415887+holden-truman@users.noreply.github.com>
2025-02-06 01:28:30 +0000

    saving

--- fit_locus.py
+++ fit_locus.py
@@ -1254,7 +1254,12 @@ def fit(table, input_info_unsorted, mag_locus,
 		#for [c1_1, c1_2], [c2_1,c2_2] in index_list[:number_of_plots]: 
         #print(c1_2)
                 for [c1_1, c1_2], [c2_1,c2_2] in index_list: 
+                    c1_band1 = c1_1['mag']
+                    c1_band2 = c1_2['mag']
+                    c2_band1 = c2_1['mag']
+                    c2_band2 = c2_2['mag']
                     print(c1_band1, c1_band2, c2_band1, c2_band2)
+
                 exit()
                 for [c1_1, c1_2], [c2_1,c2_2] in index_list: 
 
