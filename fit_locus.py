a7ac0eaf7abab2e60d3c805f323daba064fa5d5e
Holden Truman <143415887+holden-truman@users.noreply.github.com>
2025-02-09 00:12:03 +0000

    saving

--- fit_locus.py
+++ fit_locus.py
@@ -1495,7 +1495,7 @@ def fit(table, input_info_unsorted, mag_locus,
             #print("HERE")
             #time.sleep(10)            
             #exit()
-            if False:
+            if True:
                 initial_offset = .1
                 new_offset = scipy.optimize.fmin(optimize_offset_errfunc,initial_offset,maxiter=10000,maxfun=100000,ftol=0.00001,xtol=0.00001,args=(pinit,)) #holden# could change parameters of this to make abs quicker
                 out = [x + new_offset for x in pinit]
