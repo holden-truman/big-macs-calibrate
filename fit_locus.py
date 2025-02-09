5409a22443e0a84b28be9aef99d37200f859e309
Holden Truman <143415887+holden-truman@users.noreply.github.com>
2025-02-09 21:56:11 +0000

    saving

--- fit_locus.py
+++ fit_locus.py
@@ -577,6 +577,9 @@ def run(file,columns_description,output_directory=None,plots_directory=None,exte
                     if len(parts) >= 4:  # Ensure it's a valid line containing band info
                         band = parts[0]  # first thing is band, only part we need here
                         band_order.append(band)
+            
+            print(band_order)
+            exit()
 
             bands = [None] * len(band_order)
             zps = [None] * len(band_order)
