45103382b33d20dd68f545dd759c0877c369bfa2
Holden Truman <143415887+holden-truman@users.noreply.github.com>
2025-05-28 22:41:17 +0000

    saving

--- fit_locus.py
+++ fit_locus.py
@@ -1114,8 +1114,8 @@ def fit(table, input_info_unsorted, mag_locus,
                 print('iteration', itr)
                 
                 #holden# toggle plots
-                #if iteration == 'full' and (itr % plot_iteration_increment == 0 or savefig is not None):
-                #    plot_progress(pars,stat_tot,savefig)
+                if iteration == 'full' and (itr % plot_iteration_increment == 0 or savefig is not None):
+                    plot_progress(pars,stat_tot,savefig)
                 itr += 1
 
                 if residuals:
